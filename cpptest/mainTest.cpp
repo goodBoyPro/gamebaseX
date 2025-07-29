9093ea9f52874326929833317fd49f6eb0026327
xiaoming <xiaoming@qq.com>
2025-07-29 12:31:45 +0800

    周二2025-07-29 12:31 Windows平台

--- cpptest/mainTest.cpp
+++ cpptest/mainTest.cpp
@@ -1,11 +1,15 @@
 #include"stdio.h"
 #include"fstream"
-#include"string"
+#include "string"
+#include"iostream"
 int main(){
-    printf("cpptest");
     std::ifstream ifile;
-    ifile.open("fileTest.txt",std::ios::binary);
-    ifile.read(char *Str, streamsize Count)
-    printf(str.c_str());
+    ifile.open("fileTest.txt", std::ios::binary);
+    char c[10]={0};
+    std::streambuf *buf = ifile.rdbuf();
+    buf->sgetn(c, 9);
+    std::cout << c << std::endl;
+    buf->sgetn(c, 9);
+    std::cout << c << std::endl;
     return 0;
 }
