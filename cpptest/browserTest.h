22f70b32702f8f489546fb2f4c918f2b7afae46a
xiaoming <xiaoming@qq.com>
2025-10-12 16:18:52 +0800

    周日2025-10-12 16:18 Windows平台

--- cpptest/browserTest.h
+++ cpptest/browserTest.h
@@ -4,6 +4,7 @@
 #include "stdio.h"
 class BrowserTest {
 public:
+  int selectedIndex = -1;
   ImVec2 itemSize = {50, 50};
   float distance = 15;
   ImVec2 topLeft = {10, 10};
@@ -18,14 +19,22 @@ public:
       ImGui::Text("test1");
       ImGui::Separator();
       if (ImGui::BeginChild("child")) {
-        columnCount=ImGui::GetWindowWidth()/(distance+itemSize.x);
+        columnCount = ImGui::GetWindowWidth() / (distance + itemSize.x);
         for (int i = 0; i < 50; i++) {
-          ImVec2 pos=getItemPos(i);
+          ImVec2 pos = getItemPos(i);
           ImGui::SetCursorPos(pos);
           ImGui::PushID(i);
-          ImGui::Selectable("", false, 0, itemSize);
-          ImGui::SetCursorPos({pos.x,pos.y+itemSize.y-10});
-          ImGui::Text("%d",i);
+          bool isSelected = (i == selectedIndex);
+          if (ImGui::Selectable("", isSelected, 0, itemSize)) {
+            selectedIndex = i;
+          };
+          ImGui::SetCursorPos({pos.x, pos.y + itemSize.y - 10});
+          if (isSelected) {
+            ImGui::TextColored({255,0,255,255},"%d", i);
+          } else {
+            ImGui::Text("%d", i);
+          }
+
           ImGui::PopID();
         }
 
