960e5dce8196fab5ed8b631bfeabbc89a21a28f1
xiaoming <xiaoming@qq.com>
2025-12-10 18:38:39 +0800

    dev2

--- Engine/gameFramework/public/GDebug2.h
+++ Engine/gameFramework/public/GDebug2.h
@@ -4,30 +4,29 @@
 #include <base/gameWindow.h>
 #include <base/textDrawer.h>
 #include <mutex>
-#include<vector>
+#include <vector>
 #define GDEBUG_STRINGSIZE 256
 class GDebug2 {
 private:
-  
-  std::vector<std::string>texts;  
+  std::vector<std::string> texts;
   void debugDisplay(GameWindow &window_) {
+    std::lock_guard<std::mutex> lock(mut);
     int index = 0;
-    for(auto& a:texts)
-    { 
+    for (auto &a : texts) {
       GameStatics::getTextDrawer().printText(window_, a, 0, 30 * index);
       index++;
-    }   
+    }
+    texts.clear();
   }
-  std::mutex mtxDebugs2___;
+  std::mutex mut;
 
 public:
   friend class GameWindow;
-  GDebug2()=default;
-  virtual ~GDebug2()=default;
-  void printDebug(const std::string&text){
-    std::unique_lock lk(mtxDebugs2___);
-    texts.push_back(text);
+  GDebug2() = default;
+  virtual ~GDebug2() = default;
+  void printDebug(std::string text) {
+    std::unique_lock lk(mut);
+    texts.push_back(std::move(text));
   }
-  
 };
 #endif // GDEBUG2_H
